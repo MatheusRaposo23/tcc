ba7c74a3852dcab6420d92d78674e1157a758b96
MatheusRaposo23 <matheus_raposo1999@hotmail.com>
2020-10-10 13:39:38 -0300

    Projeto tcc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EquipeComponent} from './equipe/equipe.component';
+import {HomeComponent} from './home/home.component';
+import {ContatoComponent} from './contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'equipe',  component: EquipeComponent },
+  { path: 'home' , component: HomeComponent },
+  { path: 'contato', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+
+import { ProjetoComponent } from './projeto/projeto.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { DepoimentosComponent } from './depoimentos/depoimentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetoComponent,
+    HomeComponent,
+    ContatoComponent,
+    DepoimentosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
